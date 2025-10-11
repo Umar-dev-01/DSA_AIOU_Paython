2e0ea46bfc7e36eb7aecdf5d379b9bf54a2c2f4a
Umar.dev <01.dev.umar@gmail.com>
2025-10-11 12:12:59 +0000

    task

---
+++ python-practice/index.py
@@ -0,0 +1,36 @@
+#1: Write a Python program to print your name on the screen.
+print("Muhammad Umar")
+
+
+#2: Take a message as input from the user and display it on the screen.
+ message = input("Enter your message: ")
+print("You entered:", message)
+
+
+#3: Write a Python program to take two numbers as input and print their sum.
+ num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1 + num2
+print("Sum is:", sum)
+
+
+#4: Create variables of different data types (string, integer, float, boolean) and print their types.
+Ans: name = "Umar"
+age = 20
+height = 5.9
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+
+#5: Write a program to calculate the area of a circle. (Take radius as input from the user.)
+Ans: radius = float(input("Enter radius of the circle: "))
+area = 3.1416 * radius * radius
+print("Area of the circle is:", area)
+
+
+
+
