85a0863cd090e3ee55cb9a27181702670711ca64
Umar.dev <01.dev.umar@gmail.com>
2025-11-19 17:25:07 +0000

    maie

---
+++ DSA/taskw.py
@@ -0,0 +1,56 @@
+1. Write a program to sort an array of 5 integers using Bubble Sort.
+Input: 5 2 9 1 3
+Output: 1 2 3 5 9
+
+2. Modify Bubble Sort to sort an array in descending order instead of ascending.
+Example:
+Input → 3 8 1 6 2
+Output → 8 6 3 2 1
+
+3. Count the total number of swaps performed during Bubble Sort.
+Example:
+Input → 5 1 4 2 8
+Output → Sorted array: 1 2 4 5 8
+Total swaps: 4
+
+4. Implement optimized Bubble Sort (stop early if no swaps occur in a pass).
+Print how many passes it took to fully sort the array.
+
+Example:
+Input → 1 2 3 4 5
+Output →
+
+Array sorted in 1 pass.
+Sorted array: 1 2 3 4 5
+
+5. Write a Bubble Sort program that sorts floating-point numbers.
+Example:
+Input → 4.2 1.1 3.5 2.7
+Output → 1.1 2.7 3.5 4.2
+
+6. Use Bubble Sort to arrange characters alphabetically.
+Example:
+Input → ['d', 'a', 'c', 'b']
+Output → ['a', 'b', 'c', 'd']
+
+7. Modify Bubble Sort to sort only a specific range (e.g., between index 2 and 6).
+Example:
+Input → [9, 3, 8, 1, 4, 7, 2]
+Sort only from index 2 to 6 → [9, 3, 1, 2, 4, 7, 8]
+Assignment
+Manzar Malik posted a new assignment: Quiz No 1
+Posted Nov 6
+Assigned
+Read this Code:
+
+
+students = {
+"Ali": {"age": 20, "grade": "A"},
+"Sara": {"age": 19, "grade": "B"},
+"Usman": {"age": 21, "grade": "A"}
+}
+
+# Update Sara's grade and add a new student
+students["Sara"]["grade"] = "A+"
+students["Hina"] = {"age": 22, "grade": "B"}
+
